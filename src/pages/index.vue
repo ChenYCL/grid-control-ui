aa26c5f49c617cc19751c2db7a34b427d4b659d7
light <light@Macbook.local>
2022-07-30 12:37:15 +0800

    init

---
+++ src/pages/index.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+  beforeRouteEnter(to, from, next) {
+    // Handle auth logic here, redirect to login or home if logged in
+    let isLogin = localStorage.getItem('isLogin')
+    console.log('isLogin',isLogin)
+    if(!isLogin) next('/login')
+    next('/index')
+  },
+})
+</script>
