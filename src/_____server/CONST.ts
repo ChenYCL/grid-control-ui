1969eb4504607ce4cb8fd9188f61cfc980581b4b
fc01 <910677221@qq.com>
2022-08-01 10:53:57 +0800

    ···

--- src/_____server/CONST.ts
+++ src/_____server/CONST.ts
@@ -1,5 +1,5 @@
 export const CONST = {
-  binanceAPIKey: '' || localStorage.getItem('apikey'),
-  binanceSecretKey: '' || localStorage.getItem('secretKey'),
+  binanceAPIKey: 'apY1HTkTnSKnbFyLJ0JVfh5rvLQHFpmQnjeN0NRvL6KFT5OJwOqL2oIQYOtLMUt8',
+  binanceSecretKey: 'UJH641zQigd7y3oL0tsWnT6RcywdBKr0T9VFgvDbyxGlGqPby0CQhgf6t2sGKE3x',
   简易密码: 'vFSDGwsdftgqfvasd23',
 }
